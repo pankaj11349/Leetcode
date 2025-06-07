1ea1b22ef387e0757d85954380603dd2f1290944
Pankaj Tiwari <22BCS11349@cuchd.in>
2025-06-07 22:31:29 +0530

    Time: 0 ms (100%), Space: 13 MB (92.31%) - LeetHub

---
+++ 2185-counting-words-with-a-given-prefix/2185-counting-words-with-a-given-prefix.cpp
@@ -0,0 +1,9 @@
+class Solution {
+ public:
+  int prefixCount(vector<string>& words, string pref) {
+    return ranges::count_if(
+        words, [&](const string& word) { return word.find(pref) == 0; });
+  }
+};
+
+
